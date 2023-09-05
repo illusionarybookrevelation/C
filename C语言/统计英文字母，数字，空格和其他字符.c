d411affd8390ff89b0e2c77911d06de8fc0a597e
Pity Sentient beings <118274136+illusionarybookrevelation@users.noreply.github.com>
2023-09-05 17:16:00 +0800

    Add files via upload

---
+++ C语言/统计英文字母，数字，空格和其他字符.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int letters=0,space=0,digit=0,other=0;
+    char era;
+    printf("请输入一行字符：");
+    fflush(stdout);
+    while((era=getchar()) != '\n'){
+        if(era >= 'a' && era <= 'z' || era >= 'A' && era <= 'Z'){
+            letters++;
+        }else if(era >= '1' && era <= '9'){
+            space++;
+        }else if(era == ' '){
+            digit++;
+        }else{
+            other++;
+        }
+    }
+    printf("这段字符传中的英文字母有 %d个，数字有 %d个，空格有 %d个，其他字符有 %d个",letters,space,digit,other);
+    return 0;
+}
