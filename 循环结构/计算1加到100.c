3ed6684d3383ac6592ce7a98e0b1bcf1a12b7907
Pity Sentient beings <2804659229@qq.com>
2023-09-22 17:05:21 +0800

    循环

--- 循环结构/计算1加到100.c
+++ 循环结构/计算1加到100.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int i=1,sum1=0,j=1,sum2=0;
+    int i=1,sum1=0,j=1,sum2=0,k,sum3=0;
     while(i<=100){
         sum1 = sum1 + i;
         i++;
@@ -13,7 +13,11 @@ int main()
         j++;
     }while(j<=100);
 
+    for(k=1;k<=100;k++){
+        sum3 += k;
+    }
     printf("1+...+100等于(while循环)：%d\n",sum1);
-    printf("1+...+100等于(do...while循环)：%d",sum2);
+    printf("1+...+100等于(do...while循环)：%d\n",sum2);
+    printf("1+...+100等于(for循环)：%d\n",sum3);
     return 0;
 }
