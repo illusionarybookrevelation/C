4b38b07f7e08b3b70cd124367f698b0f2951980d
Pity Sentient beings <2804659229@qq.com>
2023-10-20 17:42:48 +0800

    ...

--- 数组/冒泡排序法.c
+++ 数组/冒泡排序法.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-
+#define N 10
 int main()
 {
-    int arr[10];
+    int arr[N];
     int i,j,t,k;
     printf("input 10 numbers:");
     fflush(stdout);
-    for(i=0;i<10;i++){
+    for(i=0;i<N;i++){
         scanf("%d",&t);
         arr[i] = t;
     }
-    for(i=0;i<10;i++){
-        for(j=0;j<10;j++){
+    for(i=0;i<N;i++){
+        for(j=0;j<N-i;j++){
             if(arr[j] > arr[j+1]){
                 k = arr[j];
                 arr[j] = arr[j+1];
@@ -20,7 +20,7 @@ int main()
         }
     }
     printf("the sorted numbers: \n");
-    for(i=0;i<10;i++){
+    for(i=0;i<N;i++){
         printf("%d ",arr[i]);
     }
     return 0;
