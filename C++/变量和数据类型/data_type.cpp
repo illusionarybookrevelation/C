a4f7dccddf21c62aa83618c338f058ce63ac9a23
Pity Sentient beings <2804659229@qq.com>
2023-12-19 19:23:07 +0800

    ...

--- C++/变量和数据类型/data_type.cpp
+++ C++/变量和数据类型/data_type.cpp
@@ -19,5 +19,26 @@ int main()
     long long a4 = 20;
     cout << "a = " << a4 << endl;
     cout << "a的长度为：" << sizeof a4 << endl;
+
+    //无符号整型
+    unsigned short s1 = 40000;
+    cout << "s1 = " << s1 << endl;
+
+    //字符类型
+    char c = 65;
+    cout << "c = " << c << endl;
+    char c2 = c + 1;
+    cout << "c2 = " << c2 << endl;
+
+    //布尔类型
+    bool b1 = true;
+    cout << "b1 = " << b1 << endl;
+    cout << "b1的长度：" << sizeof b1 << endl;
+
+    //浮点类型
+    float f = 2.5;
+    double d = 3.79E-23;
+    cout << "f = " << f << endl;
+    cout << "d = " << d << endl;
     cin.get();
 }
