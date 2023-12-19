47ee2f49a1a6a1804799687624c68beca04d7263
Pity Sentient beings <2804659229@qq.com>
2023-12-19 22:56:19 +0800

    ...

--- C++/变量和数据类型/data_type.cpp
+++ C++/变量和数据类型/data_type.cpp
@@ -25,7 +25,7 @@ int main()
     cout << "s1 = " << s1 << endl;
 
     //字符类型
-    char c = 65;
+    char c = 'A';
     cout << "c = " << c << endl;
     char c2 = c + 1;
     cout << "c2 = " << c2 << endl;
@@ -40,5 +40,48 @@ int main()
     double d = 3.79E-23;
     cout << "f = " << f << endl;
     cout << "d = " << d << endl;
+
+    //字面值常量
+    //整型
+    30;
+    036L;
+    0x1ELL;
+
+    //浮点型
+    3.14f;
+    1.25L;
+
+    //字符类型
+    ',';
+    'A';
+    'Z';
+    "ABCDE";
+    '\'';
+
+    //转义字符
+    char tc = '\n';
+    cout << "tc = " << tc << endl;
+    cout << "Hello World!\t\"Hello C++!\"\n\?" << endl;
+
+    //赋值时的自动类型转换
+    //整数值赋给bool类型
+    bool bi = 25;   //bi为true。打印为1
+    cout << "bi = " << bi << endl;
+
+    //bool类型赋值给算术整型
+    short s = false;    //s值为0
+    cout << "s = " << s << endl;
+
+    //浮点数赋给整数类型
+    int i = 3.14;   //i为3
+    cout << "i = " << i << endl;
+
+    //整数值赋给浮点类型
+    float fi = 10;  //fi值为10.0，打印为10
+    cout << "fi = " << fi << endl;
+
+    //赋值超出了整型范围
+    unsigned short u = 65536;
+    cout << "u = " << u << endl;
     cin.get();
 }
