31b69f6d9158e04b2e3bd0586c9856dbbf805c64
Pity Sentient beings <2804659229@qq.com>
2024-03-04 16:07:15 +0800

    ...

--- C语言/n的阶乘-for.c
+++ C语言/n的阶乘-for.c
@@ -5,7 +5,7 @@ int main()
 	int sum=0;
 	int n;
 	int k=1;
-	printf("璇疯緭鍏ユ暟瀛楋細");
+	printf("请输入数字：");
 	scanf("%d",&n);
 	for(int i=1;i<n+1;i++){
 		
@@ -14,6 +14,6 @@ int main()
 		
 	}
 	
-	printf("缁撴灉鏄細%d",sum);
+	printf("结果是：%d",sum);
 	return 0;
 }
